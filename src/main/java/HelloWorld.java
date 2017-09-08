f2ce42c7f553d571433a05642205a388bac6ee1b
hanyuwang <1093615441@qq.com>
2017-09-09 04:12:27 +0800

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,21 +1,40 @@
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 
 /**
  * Created by Hanyu on 2017/8/9.
  */
-public class HelloWorld {
+public class HelloWorld implements Watcher {
     private static final int TIME_OUT = 3000;
     private static final String HOST = "192.168.147.128:2181";
-
+    private static ZooKeeper zookeeper = null;
     public static void main(String[] args) throws Exception {
-        ZooKeeper zookeeper = new ZooKeeper(HOST, TIME_OUT, null);
+        Watcher w = new HelloWorld();
+        zookeeper = new ZooKeeper(HOST, TIME_OUT, w);
         if(zookeeper.exists("/test", false) == null) {
             zookeeper.create("/test", "znode1".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         }
-        System.out.println(new String(zookeeper.getData("/test", false, null)));
+
+        Stat stat = new Stat();
+
+        for(int i = 0; i < 20; i++) {
+            zookeeper.getData("/test", true, stat);
+            System.out.println(stat.getVersion());
+            zookeeper.setData("/test", "hanyu旱雨".getBytes(), stat.getVersion());
+        }
+
+    }
+
+    public void process(WatchedEvent event) {
+        System.out.println("watcher hanyu ");
+        try {
+            zookeeper.getChildren(event.getPath(), true);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
